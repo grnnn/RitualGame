e997180735d1921b96aa5a33b0edc230c4c8945b
Taylor Owen <towenmilner@gmail.com>
2016-01-29 22:26:11 -0800

    Created and tested State Machine Module

--- src/StateMachine/State.js
+++ src/StateMachine/State.js
@@ -1,13 +1,78 @@
-export default class State
+"use strict";
+
+//Dinky little state class
+class State
 {
   constructor()
   {
+    //public
     this.transitions = [];
+    this.data = {};
+    this.update = function(state){};
+
+    //private
+    this._resetData = {};
   }
 
-  setTransition( state, conditional )
+  setTransition( state /*: State*/, conditional /*:bool(State)*/, priority /*:int*/)
   {
-    var transition = {state: state, conditional: conditional};
+    var transition = { targetState: state,
+                       conditional: conditional,
+                       priority: priority };
     this.transitions.push(transition);
+
+    this.transitions.sort(function(a, b){
+      return a.priority - b.priority;
+    });
+  }
+
+  setTransition( state /*: State*/, conditional /*:bool(State)*/)
+  {
+    var transition = { targetState: state,
+                       conditional: conditional,
+                       priority: 0 };
+    this.transitions.push(transition);
+
+    this.transitions.sort(function(a, b){
+      return a.priority - b.priority;
+    });
+  }
+
+  setData(data /*{}*/)
+  {
+    this._resetData = data;
+    this.data = data;
+  }
+
+  setUpdate(update /*: void(State)*/)
+  {
+    this.update = update;
+  }
+
+  reset()
+  {
+    this.data = this._resetData;
   }
 }
+
+/* Usage:
+var stateA = new State();
+stateA.setData({timer: 60});
+
+var stateB = new State();
+
+function exampleConditional(state)
+{
+  return state.data.timer == 0;
+}
+
+stateA.setTransition(stateB, exampleConditional);
+stateA.setUpdate(function(state){
+  state.data.timer--;
+});
+
+When the timer in stateA reaches 0, the StateMachine will transition to stateB.
+Conditionals are tested every frame. When transitioning to a new state, the
+'data' field of the old state will be reset.
+
+*/
--- src/StateMachine/StateMachine.js
+++ src/StateMachine/StateMachine.js
@@ -1,7 +1,33 @@
-import State;
+"use strict";
 
-export default class StateMachine
+class StateMachine
 {
+  constructor( currentState /*: State*/ )
+  {
+    //private
+    this._currentState = currentState;
+  }
 
+  set currentState( currentState /*: State*/)
+  {
+    this._currentState = currentState;
+  }
+
+  update()
+  {
+    this._currentState.update(this._currentState);
+
+    for (var i = 0; i < this._currentState.transitions.length; i++)
+    {
+      var transition = this._currentState.transitions[i];
+
+      // test if we transition to another state
+      if (transition.conditional(this._currentState))
+      {
+        this._currentState.reset();
+        this._currentState = transition.state;
+      }
+    }
+  }
 
 }
